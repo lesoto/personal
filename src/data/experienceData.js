901cff765bea1e58dd818f366027fde619e79d6c
lesoto <lesoto@lesoto.com>
2022-10-29 18:02:13 -0400

    101 - added eslint

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,10 +1,9 @@
 export const experienceData = [
-    {
-        id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
-    },
-    
-]
+  {
+    id: 1,
+    company: "Globex Corporation",
+    jobtitle: "Frontend Developer",
+    startYear: "2018",
+    endYear: "2019",
+  },
+];
